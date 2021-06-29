1339a62d76b3b693c4510e82739630f52881bac7
ariverak <jrivera.v@outlook.com>
2021-06-28 21:17:14 -0400

    send user logged to users microservice

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
 import { validateLogin } from '../validator';
+import axios from 'axios';
+import * as queueService from '../services/queue-service';
 
 export const login = async (req: Request, res: Response) => {
   const data = req.body;
   const valid = validateLogin(data);
-  if (!valid)
+  if (!valid) {
     res.status(400).json({ success: false, errors: validateLogin.errors });
-
-  res.send([]);
+  }
+  const { rut } = data;
+  try {
+    const user = await axios.get(`${process.env.USERS_API_URL}/${rut}`);
+    await queueService.push(JSON.stringify(user.data));
+    res.json({ success: true, message: 'login successful' });
+  } catch (err) {
+    res.json({ success: false, errors: [err.message] });
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { login } from './auth';
-import * as queueService from './services/queue-service';
-
 dotenv.config();
+
 const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 
 //Set all routes from routes folder
-app.use('/login', login);
+app.post('/login', login);
 
 // start the express server
 app.listen(port, () => {
-  queueService.push('Hello');
   console.log(`server started at http://localhost:${port}`);
 });
